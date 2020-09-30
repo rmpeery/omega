5692d6bde2ff8b9e28c9d8565d474498b3579486
rmpeery <rmpeery@gmail.com>
2020-09-30 13:39:39 -0600

    Update loop_fsa2phy.pl

--- loop_fsa2phy.pl
+++ loop_fsa2phy.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/perl -w
 # R Peery 30 Sept 2020
+# converting fasta to phylip with script
+# obtained from Yu-Wei's Bioinformatics playground 
+# http://yuweibioinfo.blogspot.com/2009/01/fasta-to-phylip-converter.html
+# fsa2phy.pl
+
+
 use strict;
 
 opendir(DIR, ".");
