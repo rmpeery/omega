c46c2922ad7c00f8db23b4fe632e015918f083f8
rmpeery <rmpeery@gmail.com>
2020-09-30 11:03:46 -0600

    Create loop_FileNameOmegaValue.pl

---
+++ loop_FileNameOmegaValue.pl
@@ -0,0 +1,19 @@
+#!/opt/local/bin/perl -w
+# scripted on 30 Sept 2020 by R Peery
+
+use strict;
+
+opendir(DIR, ".");
+my @files = grep(/\.txt$/,readdir(DIR));
+closedir(DIR);
+
+my $file = ();
+
+foreach $file (@files) {
+	my $cmd = "ls $file >> omegaSummary.txt | grep 'omega (dN/dS) =  ' $file >> omegaSummary.txt";
+	system($cmd);
+	}
+print "done\n";
+
+#rna-XM_027980314.1_algn.fasta.phy.cml.out.txt
+#ls rna-XM_027980314.1_algn.fasta.phy.cml.out.txt >> omegaSummary.txt | grep 'omega (dN/dS) =  ' rna-XM_027980314.1_algn.fasta.phy.cml.out.txt >> omegaSummary.txt
