cca763178bd5d8dd57329ea36803bf05393e2bea
rmpeery <rmpeery@gmail.com>
2020-11-16 09:56:00 -0700

    Update loop_codeml.pl

--- loop_codeml.pl
+++ loop_codeml.pl
@@ -22,12 +22,70 @@ my $tre = shift;
 
 # settings for one value per gene based on suggestion here:
 # https://www.researchgate.net/topic/PAML
+
+#had to mkdir codemlOut before script would run
+
 foreach $file (@files) {
 	open OUT, ">$file.codeml.ctl";
-	print OUT ("seqfile = nucl_aln_phy/$file\ntreefile = treeFiles/$tre\noutfile = codemlOut/$file.cml.out.txt\nnoisy = 9\nverbose = 1\nrunmode = 0\nseqtype = 1\nCodonFreq = 2\nclock = 0\naaDist = 0\naaRatefile = wag.dat\nmodel = 0\nNSsites = 0\nicode = 0\nMgene = 0\nfix_kappa = 0\nkappa = 1\nfix_omega = 0\nomega = 1\nfix_alpha = 1\nalpha = 0\nMalpha = 0\nncatG = 10\ngetSE = 0\nRateAncestor = 0\nSmall_Diff = .5e-6\ncleandata = 1\nfix_blength = 0\nmethod = 0\n");
-	my $cmd = "./codeml $file.codeml.ctl";
+	print OUT ("seqfile = path/$file
+	treefile = path/$tre
+	outfile = path/$file.cml.out.txt
+	noisy = 9
+	verbose = 1
+	runmode = 0
+	seqtype = 1
+	CodonFreq = 2
+	clock = 1
+	aaDist = 0
+	aaRatefile = wag.dat
+	model = 0
+	NSsites = 0
+	icode = 0
+	Mgene = 0
+	fix_kappa = 0
+	kappa = 1
+	fix_omega = 0
+	omega = 1
+	fix_alpha = 1
+	alpha = 0
+	Malpha = 0
+	ncatG = 10
+	getSE = 0
+	RateAncestor = 0
+	Small_Diff = .5e-6
+	cleandata = 1
+	fix_blength = 0\n");
+	my $cmd = "path/codeml $file.codeml.ctl";
 	system($cmd);
 	close OUT;
 	}
 print "done\n";
 
+#seqfile = path/infile.phy
+#treefile = path/treefile.tre
+#outfile = path/outfile.cml.out.txt
+#noisy = 9 * what to print to screen 9 = a lot
+#verbose = 1 * detailed output
+#runmode = 0 * user defined tree
+#seqtype = 1 * codons
+#CodonFreq = 2 * F3X4
+#clock = 1 * clock, rooted tree
+#aaDist = 0
+#aaRatefile = wag.dat
+#model = 0 * one omega ratio for all branches
+#NSsites = 0 * one omega ratio
+#icode = 0 * universal code
+#Mgene = 0 * rates
+#fix_kappa = 0 * estimated
+#kappa = 1 * initial kappa
+#fix_omega = 0 * estimated
+#omega = 1 * initial omega
+#fix_alpha = 1
+#alpha = 0
+#Malpha = 0
+#ncatG = 10 * of site categories for M7 and M8
+#getSE = 0 * don't want SE est
+#RateAncestor = 0
+#Small_Diff = .5e-6
+#cleandata = 1 * remove sites with ambiguity data
+#fix_blength = 0 * ignore
